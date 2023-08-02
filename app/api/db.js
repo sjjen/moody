a8de19b5d6b8485860d641b4e03d7ca8b2e7d4d5
sjjen <sjjen.lee@gmail.com>
2023-08-02 14:46:40 -0700

    setting up project

---
+++ app/api/db.js
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
+
+export default pool;
---
+++ app/login/page.js
@@ -0,0 +1,9 @@
+import styles from "../styles/login.module.css";
+
+export default function Login() {
+  return (
+    <main>
+      <h1>login</h1>
+    </main>
+  )
+}
