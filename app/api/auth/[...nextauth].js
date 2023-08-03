45334667992a821c94a19b8b44f831aca6dbd1f9
sjjen <sjjen.lee@gmail.com>
2023-08-02 17:55:23 -0700

    starting auth

---
+++ app/api/auth/[...nextauth].js
@@ -0,0 +1,25 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // Add more providers here if needed
+  ],
+  // Optional configurations
+  session: {
+    jwt: true,
+  },
+  callbacks: {
+    async jwt(token, user) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+});
--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './styles/page.module.css'
+
 
 export default function Home() {
+
+  
   return (
-    <main className={styles.main}>
-      <p>Yo</p>
-    </main>
-  )
+    <div>Hello, This is home!</div>
+  );
 }
